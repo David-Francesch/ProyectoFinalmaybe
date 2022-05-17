83ffca48a895a9e3abb496b889f19986e773ec3f
david-francesch <david.francesch.colomer@gmail.com>
2022-05-17 09:32:16 +0200

    mete todo bien en la matriz y esta separado en clases y formateado

---
+++ ProyectoFinalMaybe/src/Matriz.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Matriz {
+    Scanner sc = new Scanner(System.in);
+    private char[][] matriz;
+
+
+    public Matriz() {
+        this.matriz = new char[5][5];
+
+        rellenarMatriz(limpiarClave());
+    }
+
+    public String limpiarClave() {
+        String limpia = "";
+        System.out.println("Escribe la palabra con la que quieres empezar:");
+        String palabra = sc.nextLine().toUpperCase();
+
+        for (int i = 0; i < palabra.length() - 1; i++) {
+            int contpa = 0;
+            for (int j = 0; j < limpia.length(); j++) {
+                if (limpia.charAt(j) == palabra.charAt(i)) {
+                    contpa++;
+                }
+            }
+            if (contpa <= 0) {
+                limpia += palabra.charAt(i);
+            }
+        }
+        return limpia;
+    }
+
+    public void rellenarMatriz(String limpia) {
+        int x = 0;
+        char letra = 'A';
+        int cont = 0;
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                //mete la palabra limpia
+                if (x < limpia.length()) {
+                    matriz[i][j] = limpia.charAt(x);
+                    x++;
+                }else{
+                    // Despues de meter la palabra limpia
+                        while (checkRepetidos(limpia, letra) || letra == 'J' ) {
+                        letra++;
+                    }
+                        matriz[i][j] = letra;
+                        letra++;
+                }
+            }
+                
+        }
+    }
+    
+
+    public boolean checkRepetidos(String limpia, char letra) {
+        boolean esta = false;
+
+        for (int i = 0; i < limpia.length(); i++) {
+            if (limpia.charAt(i) == letra) {
+                esta = true;
+                return esta;
+            }
+        }
+        
+        return esta;
+    }
+
+    @Override
+    public String toString() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return null;
+    }
+}
